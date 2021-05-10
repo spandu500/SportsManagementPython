ebc5c86b7e6e7f5342345af48379abf03abeaaa2
Spandan Ghosh <spandu500@gmail.com>
2021-05-10 16:19:31 +0530

    add bogus code

--- main.py
+++ main.py
@@ -78,7 +78,132 @@ def ecoach():
 #     shop.title("Shop Items")
 #     cart()
 
+# def __init__(self):
+#         __init__()
+#         self.shoppinglist = []
+#         self.adding = True
+#         self.initUI()
 
+
+# def initUI(self):
+#     def press():
+#         sc.delete("1.0", tk.END)
+#         sc.insert(tk.END, getList(self))
+
+#     def addpress():
+#         self.adding=not self.adding
+#         if self.adding==True:
+#             ar['text'] = "Click to Start \r Removing"
+#             press()
+#         else:
+#             ar['text']="Click to Start \r Adding"
+#             press()
+
+#     def shop(s):
+#         if(self.adding==True):
+#             self.shoppinglist.append(s)
+#         else:
+#             if s in self.shoppinglist:
+#                 self.shoppinglist.remove(s)
+                
+
+#     self.master.title("Shop Items")
+
+#     spike = tk.Button(self, command=lambda:[shop("Spike Shoes"),press()])
+#     image = ImageTk.PhotoImage(file="shop/spike.jpg")
+#     spike.config(image=image, width=200, height=200, bg="white")
+#     spike.image = image
+#     spike.grid(row=2, column=1)
+
+#     ball = tk.Button(self, command=lambda:[shop("Ball 1"),press()])
+#     image = ImageTk.PhotoImage(file="shop/ball.jpg")
+#     ball.config(image=image, width=200, height=200, bg="white")
+#     ball.image = image
+#     ball.grid(row=2, column=2)
+
+#     bat = tk.Button(self, command=lambda: [shop("Bat"),press()])
+#     image = ImageTk.PhotoImage(file="shop/bat.jpg")
+#     bat.config(image=image, width=200, height=200, bg="white")
+#     bat.image = image
+#     bat.grid(row=2, column=3)
+
+#     cones = tk.Button(self, command=lambda:[shop("Cones"),press()])
+#     image = ImageTk.PhotoImage(file="shop/cones.jpg")
+#     cones.config(image=image, width=200, height=200, bg="white")
+#     cones.image = image
+#     cones.grid(row=2, column=4)
+
+#     gloves = tk.Button(self, command=lambda: [shop("Gloves"), press()])
+#     image = ImageTk.PhotoImage(file="shop/gloves.jpg")
+#     gloves.config(image=image, width=200, height=200, bg="white")
+#     gloves.image = image
+#     gloves.grid(row=3, column=1)   
+
+#     football = tk.Button(self, command=lambda: [shop("Football"), press()])
+#     image = ImageTk.PhotoImage(file="shop/football.jpg")
+#     football.config(image=image, width=200, height=200, bg="white")
+#     football.image = image
+#     football.grid(row=3, column=2)
+
+#     raquet = tk.Button(self, command=lambda: [shop("Tennis Raquet"), press()])
+#     image = ImageTk.PhotoImage(file="shop/raquet.jpg")
+#     raquet.config(image=image, width=200, height=200, bg="white")
+#     raquet.image = image
+#     raquet.grid(row=3, column=3)
+
+#     jersey1 = tk.Button(self, command=lambda: [shop("Orange Jersey"), press()])
+#     image = ImageTk.PhotoImage(file="shop/jersey1.jpg")
+#     jersey1.config(image=image, width=200, height=200, bg="white")
+#     jersey1.image = image
+#     jersey1.grid(row=3, column=4)
+
+#     ball2 = tk.Button(self, command=lambda: [shop("Season Ball"), press()])
+#     image = ImageTk.PhotoImage(file="shop/ball2.jpg")
+#     ball2.config(image=image, width=200, height=200, bg="white")
+#     ball2.image = image
+#     ball2.grid(row=4, column=1)
+
+#     shorts = tk.Button(self, command=lambda: [shop("Outer Shorts"), press()])
+#     image = ImageTk.PhotoImage(file="shop/shorts.jpg")
+#     shorts.config(image=image, width=200, height=200, bg="white")
+#     shorts.image = image
+#     shorts.grid(row=4, column=2)
+
+#     jersey2 = tk.Button(self, command=lambda: [shop("Yellow Jersey"), press()])
+#     image = ImageTk.PhotoImage(file="shop/jersey2.jpg")
+#     jersey2.config(image=image, width=200, height=200, bg="white")
+#     jersey2.image = image
+#     jersey2.grid(row=4, column=3)
+
+#     socks = tk.Button(self, command=lambda: [shop("Long Socks"), press()])
+#     image = ImageTk.PhotoImage(file="shop/socks.jpg")
+#     socks.config(image=image, width=200, height=200, bg="white")
+#     socks.image = image
+#     socks.grid(row=4, column=4)
+
+#     sc = tk.Text(self, height=13, width=30)
+#     sc.insert(tk.END, getList(self))
+#     sc.grid(row=2, column=5)
+
+#     ar = tk.Button(self, text="Click to start \r Removing", bg="darkblue", fg="white", font='Helvetica 18 bold', width=15, height=5, command=lambda: [addpress()])
+#     ar.grid(row=3, column=5)
+
+#     quit = tk.Button(self, text="QUIT", fg="white", bg="red", font='Helvetica 18 bold', command=self.master.destroy, width=10, height=5)
+#     quit.grid(row=4, column=5)
+
+#     self.pack()
+
+# def getList(self):
+#     items='Your Shopping Cart Contains: \n'
+#     for item in self.shoppinglist:
+#         items+= item + "\n"
+#     return items
+
+# def mainshop():
+#     root = Tk()
+#     app = ShoppingCart()
+#     root.configure(background="white")
+#     root.mainloop()
 
 def Booking_History():
     hist = tk.Toplevel()
@@ -248,67 +373,67 @@ def Back_page1():
     back1 = bookingPage1()
 
 
-def bookingPage2():
-    nextlvl = tk.Toplevel()
-    nextlvl.geometry("900x900+100+100")
-    nextlvl.title("AVAILABLE GROUNDS")
-    nextlvl.configure(bg="white")
-    frame7 = Frame(nextlvl, bg="white").place(
-        x=100, y=50, height=600, width=650)
+# def bookingPage2():
+#     nextlvl = tk.Toplevel()
+#     nextlvl.geometry("900x900+100+100")
+#     nextlvl.title("AVAILABLE GROUNDS")
+#     nextlvl.configure(bg="white")
+#     frame7 = Frame(nextlvl, bg="white").place(
+#         x=100, y=50, height=600, width=650)
 
-    # 4th Ground
-    d1 = Label(nextlvl, text="Bhumiputra Maidan", font="tr 15",
-               fg="black", bg="white").place(x=200, y=120, width=300)
-    d2 = Label(nextlvl, text="ADDRESS:", font="tr 10",
-               fg="black", bg="white").place(x=120, y=150)
-    d3 = Label(nextlvl, text="Bhumiputra Maidan NEAR RAM MANDIR ",
-               font="tr 10", fg="black", bg="white").place(x=190, y=150)
-    d4 = Label(nextlvl, text="DOMBIVILI , Thane, Maharashtra 400612",
-               font="tr 10", fg="black", bg="white").place(x=190, y=170)
-    d5 = Label(nextlvl, text="Phone:", font="tr 10",
-               fg="black", bg="White").place(x=120, y=200)
-    d6 = Label(nextlvl, text="8254682025", font="tr 10 ",
-               fg="black", bg="White").place(x=180, y=200)
-    b4 = Button(nextlvl, text="Book Now", font="tr 10",
-                fg="black", bd=1, bg="#41B3A3", command=Book_now)
-    b4.place(x=300, y=220)
+#     # 4th Ground
+#     d1 = Label(nextlvl, text="Bhumiputra Maidan", font="tr 15",
+#                fg="black", bg="white").place(x=200, y=120, width=300)
+#     d2 = Label(nextlvl, text="ADDRESS:", font="tr 10",
+#                fg="black", bg="white").place(x=120, y=150)
+#     d3 = Label(nextlvl, text="Bhumiputra Maidan NEAR RAM MANDIR ",
+#                font="tr 10", fg="black", bg="white").place(x=190, y=150)
+#     d4 = Label(nextlvl, text="DOMBIVILI , Thane, Maharashtra 400612",
+#                font="tr 10", fg="black", bg="white").place(x=190, y=170)
+#     d5 = Label(nextlvl, text="Phone:", font="tr 10",
+#                fg="black", bg="White").place(x=120, y=200)
+#     d6 = Label(nextlvl, text="8254682025", font="tr 10 ",
+#                fg="black", bg="White").place(x=180, y=200)
+#     b4 = Button(nextlvl, text="Book Now", font="tr 10",
+#                 fg="black", bd=1, bg="#41B3A3", command=Book_now)
+#     b4.place(x=300, y=220)
 
-    # 5th turf
-    e1 = Label(nextlvl, text="PHOENIX GROUND", font="tr 15",
-               fg="black", bg="white").place(x=200, y=300, width=300)
-    e2 = Label(nextlvl, text="ADDRESS:", font="tr 10",
-               fg="black", bg="white").place(x=120, y=350)
-    e3 = Label(nextlvl, text="J.K turf, near midc water tank",
-               font="tr 10", fg="black", bg="white").place(x=190, y=350)
-    e4 = Label(nextlvl, text="Kalyan-Dmbivili, Thane, Maharashtra 400612",
-               font="tr 10", fg="black", bg="white").place(x=190, y=370)
-    e5 = Label(nextlvl, text="Phone:", font="tr 10",
-               fg="black", bg="White").place(x=120, y=390)
-    e6 = Label(nextlvl, text="8254682025", font="tr 10 ",
-               fg="black", bg="White").place(x=180, y=390)
-    b5 = Button(nextlvl, text="Book Now", font="tr 10",
-                fg="black", bd=1, bg="#41B3A3", command=Book_now)
-    b5.place(x=300, y=420)
+#     # 5th turf
+#     e1 = Label(nextlvl, text="PHOENIX GROUND", font="tr 15",
+#                fg="black", bg="white").place(x=200, y=300, width=300)
+#     e2 = Label(nextlvl, text="ADDRESS:", font="tr 10",
+#                fg="black", bg="white").place(x=120, y=350)
+#     e3 = Label(nextlvl, text="J.K turf, near midc water tank",
+#                font="tr 10", fg="black", bg="white").place(x=190, y=350)
+#     e4 = Label(nextlvl, text="Kalyan-Dmbivili, Thane, Maharashtra 400612",
+#                font="tr 10", fg="black", bg="white").place(x=190, y=370)
+#     e5 = Label(nextlvl, text="Phone:", font="tr 10",
+#                fg="black", bg="White").place(x=120, y=390)
+#     e6 = Label(nextlvl, text="8254682025", font="tr 10 ",
+#                fg="black", bg="White").place(x=180, y=390)
+#     b5 = Button(nextlvl, text="Book Now", font="tr 10",
+#                 fg="black", bd=1, bg="#41B3A3", command=Book_now)
+#     b5.place(x=300, y=420)
 
-    # 6th Ground
-    f1 = Label(nextlvl, text="DYANMANDIR GROUND", font="tr 15",
-               fg="black", bg="white").place(x=200, y=500, width=300)
-    f2 = Label(nextlvl, text="ADDRESS:", font="tr 10",
-               fg="black", bg="white").place(x=120, y=550)
-    f3 = Label(nextlvl, text="DYANMANDIR GROUND, near Mamta Hospital, model college",
-               font="tr 10", fg="black", bg="white").place(x=190, y=550)
-    f4 = Label(nextlvl, text="Dombivili, Thane, Maharashtra 400612",
-               font="tr 10", fg="black", bg="white").place(x=190, y=570)
-    f5 = Label(nextlvl, text="Phone:", font="tr 10",
-               fg="black", bg="White").place(x=120, y=600)
-    f6 = Label(nextlvl, text="8254682025", font="tr 10 ",
-               fg="black", bg="White").place(x=180, y=600)
-    b3 = Button(nextlvl, text="Book Now", font="tr 10",
-                fg="black", bd=1, bg="#41B3A3", command=Book_now)
-    b3.place(x=300, y=620)
+#     # 6th Ground
+#     f1 = Label(nextlvl, text="DYANMANDIR GROUND", font="tr 15",
+#                fg="black", bg="white").place(x=200, y=500, width=300)
+#     f2 = Label(nextlvl, text="ADDRESS:", font="tr 10",
+#                fg="black", bg="white").place(x=120, y=550)
+#     f3 = Label(nextlvl, text="DYANMANDIR GROUND, near Mamta Hospital, model college",
+#                font="tr 10", fg="black", bg="white").place(x=190, y=550)
+#     f4 = Label(nextlvl, text="Dombivili, Thane, Maharashtra 400612",
+#                font="tr 10", fg="black", bg="white").place(x=190, y=570)
+#     f5 = Label(nextlvl, text="Phone:", font="tr 10",
+#                fg="black", bg="White").place(x=120, y=600)
+#     f6 = Label(nextlvl, text="8254682025", font="tr 10 ",
+#                fg="black", bg="White").place(x=180, y=600)
+#     b3 = Button(nextlvl, text="Book Now", font="tr 10",
+#                 fg="black", bd=1, bg="#41B3A3", command=Book_now)
+#     b3.place(x=300, y=620)
 
-    Back1 = Button(nextlvl, text="Back", font="tr 10", fg="black",
-                   bg="#41B3A3", command=Back_page1).place(x=700, y=620)
+#     Back1 = Button(nextlvl, text="Back", font="tr 10", fg="black",
+#                    bg="#41B3A3", command=Back_page1).place(x=700, y=620)
 
 
 def equipmentShop():
@@ -331,7 +456,7 @@ def equipmentShop():
     a6 = Label(equip, text="8852023645", font="tr 10 ",
                fg="black", bg="White").place(x=180, y=200)
     b1 = Button(equip, text="Shop Now", font="tr 10",
-                fg="black", bd=1, bg="#41B3A3", command=ShoppingCart)
+                fg="black", bd=1, bg="#41B3A3", command=mainshop)
     b1.place(x=300, y=250)
 
     # 2nd Shop
@@ -348,7 +473,7 @@ def equipmentShop():
     b16 = Label(equip, text="8254682025", font="tr 10 ",
                 fg="black", bg="White").place(x=180, y=390)
     b2 = Button(equip, text="Shop Now", font="tr 10",
-                fg="black", bd=1, bg="#41B3A3", command=ShoppingCart)
+                fg="black", bd=1, bg="#41B3A3", command=mainshop)
     b2.place(x=300, y=420)
 
 
@@ -412,8 +537,8 @@ def bookingPage1():
 
     Back1 = Button(book, text="Back", font="tr 10", fg="black",
                    bg="#41B3A3", command=Back_page).place(x=650, y=620)
-    Next = Button(book, text="Next", font="tr 10", fg="black",
-                  bg="#41B3A3", command=bookingPage2).place(x=700, y=620)
+    # Next = Button(book, text="Next", font="tr 10", fg="black",
+    #               bg="#41B3A3", command=bookingPage2).place(x=700, y=620)
 
 
 def logininfo():
@@ -501,6 +626,8 @@ def registerinfo():
     RPhn.place(x=400, y=400)
     REmail = tk.Entry(reg)
     REmail.place(x=400, y=450)
+    Back1 = Button(reg, text="Back", font="tr 10", fg="black",
+                bg="#41B3A3", command=Back_page).place(x=650, y=620)
     # Rtype = tk.Combobox(text = "UserType", textvariable=type, values='1,2,3')
     # Rtype.place(x=400, y=550) 
 # #l1 = Label(UI_frame, text="Algorithm: ", bg=WHITE)
